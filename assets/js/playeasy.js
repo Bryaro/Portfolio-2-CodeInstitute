4550875a309c4234abe8c7d85944c43cc1d6a19b
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-18 18:39:55 +0000

    add webrespinsive style to .play-computer-container for all screens

--- assets/js/playeasy.js
+++ assets/js/playeasy.js
@@ -7,11 +7,11 @@ playEasyHTML = `<div>
 <div id="message">Lets go!!</div>
 <div class="player-computer-container">
 <div class="player">
-    <h2 class="scores">Your score is: <span id="player-score">0</span></h2>
+    <h2 class="scores">My score: <span id="player-score">0</span></h2>
     <img class="player-image" src="assets/images/player-head.jpg" alt="hands">
 </div>
 <div class="computer-player">
-    <h2 class="scores">Computers-score is: <span id="computer-score">0</span></h2>
+    <h2 class="scores">His score: <span id="computer-score">0</span></h2>
     <img class="computer-image" src="assets/images/computer-head.jpg" alt="hands">
 </div>
 </div>
