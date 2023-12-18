cd068609e846fedffcd6b5408d31c4238a443807
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-18 13:41:46 +0000

    add playheas.js file. very similar to playhard but here it is just three first buttons

---
+++ assets/js/playeasy.js
@@ -0,0 +1,17 @@
+playEasyHTML = `<div>
+<p>Decide your option:</p>
+<button data-option="0" aria-label="rock" class="control">Rock</button>
+<button data-option="1" aria-label="paper" class="control">Paper</button>
+<button data-option="2" aria-label="scissor" class="control">Scissors</button>
+</div>
+<div id="message">Lets go!!</div>
+<div class="player-computer-container">
+<div class="player">
+    <h2 class="scores">Your score is: <span id="player-score">0</span></h2>
+    <img class="player-image" src="assets/images/player-head.jpg" alt="hands">
+</div>
+<div class="computer-player">
+    <h2 class="scores">Computers-score is: <span id="computer-score">0</span></h2>
+    <img class="computer-image" src="assets/images/computer-head.jpg" alt="hands">
+</div>
+</div>`;
